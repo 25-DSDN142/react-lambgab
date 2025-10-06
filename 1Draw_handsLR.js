4acfc91b7c00c1d1674239c3ab272445674f6c62
lambergabr <lambertegab1210@gmail.com>
2025-10-06 14:23:15 +1300

    Adding in Assests
    Created the images on Krita and integrated them into the code. Everything works well! Assets just need a little tweaking to make it look more dramatic

--- 1Draw_handsLR.js
+++ 1Draw_handsLR.js
@@ -2,14 +2,22 @@
 // USING THE GESTURE DETECTORS (check their values in the debug menu)
 // detectHandGesture(hand) returns "Pinch", "Peace", "Thumbs Up", "Pointing", "Open Palm", or "Fist"
 
+let bgImage;
+let glowImage;
+
 /* load images here */
 function prepareInteraction() {
-  //bgImage = loadImage('/images/background.png');
+  bgImage = loadImage('/images/ShadowBG.png');
+  glowImage = loadImage('/images/Glow png.png')
+
 }
 
 function drawInteraction(faces, hands) {
   // hands part
   // for loop to capture if there is more than one hand on the screen. This applies the same process to all hands.
+  
+  image(bgImage, 0, 0);
+  
   for (let i = 0; i < hands.length; i++) {
     let hand = hands[i];
     // console.log(hand);
@@ -19,31 +27,28 @@ function drawInteraction(faces, hands) {
 
     let middleFingerMcpX = hand.middle_finger_mcp.x;
     let middleFingerMcpY = hand.middle_finger_mcp.y;
-
-    let middleFingerPipX = hand.middle_finger_pip.x;
-    let middleFingerPipY = hand.middle_finger_pip.y;
-
-    let wristX = hand.wrist.x;
-    let wristY = hand.wrist.y;
     
     /*
     Start drawing on the hands here
     */
 
+
     let whatGesture = detectHandGesture(hand)
 
     if (whatGesture == "Open Palm") {
-      DrawGatt(middleFingerMcpX, middleFingerMcpY, middleFingerPipX, middleFingerPipY)
-      strokeWeight(5);
-      fill(255, 38, 219) // pink
+
+     image(glowImage,middleFingerMcpX -120, middleFingerMcpY -110)
+     DrawGatt(middleFingerMcpX, middleFingerMcpY)
     }
+    
+
 function DrawGatt(){
 
-  let leftearX = middleFingerMcpX + 30; //+30
-  let leftearY = middleFingerMcpY + 20; //+20
+  let leftearX = middleFingerMcpX-20; //+30
+  let leftearY = middleFingerMcpY-20; //+20
 
-  let rightearX = middleFingerMcpX + 65; //+65
-  let rightearY = middleFingerMcpY + 85; //+85
+  let rightearX = middleFingerMcpX + 15; //+65
+  let rightearY = middleFingerMcpY + 45; //+85
 
   strokeWeight(3);
   stroke(0);
@@ -55,7 +60,7 @@ function DrawGatt(){
   // Add vertices.
 
   //left ear
-  vertex(leftearX, leftearY);//tip of left ear // (30,20) before
+  vertex(leftearX, leftearY);//tip of left ear
   vertex(leftearX +20, leftearY+20); //left head
 
   //right ear
@@ -95,10 +100,16 @@ endShape(CLOSE);
   ellipse(leftearX +15, leftearY +30, 3, 3); //left eye
   ellipse(rightearX+5, rightearY -35, 3,3); //right eye
 
+//nose
+  stroke(0.5)
+  strokeWeight(1);
+  fill(0);
+  ellipse(leftearX +27, leftearY +30, 3, 3); 
+
 //mouth
  strokeWeight(1);
  stroke(0);
-  line(leftearX+20, leftearY +30, rightearX, rightearY-35);
+ line(leftearX+20, leftearY +33, rightearX, rightearY-32);
   } 
 
     /*
